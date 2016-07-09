7abae11ad121948ac8e2892213cc80da1793beee
Peter Indiola <pindiola@gmail.com>
2016-07-09 08:26:40 +0800

    prod bundles

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
-import { HomePage } from './components/HomePage';
+import HomePage from './components/HomePage'; //eslint-disable-line import/no-named-as-default
 import AboutPage from './components/AboutPage';
 import WeatherDetailPage from './components/WeatherDetailPage';
 
